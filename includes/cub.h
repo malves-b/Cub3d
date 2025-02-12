919336435d59b688dca0bcbecb7b1ec155ce33c5
Matheus Alves Barbosa Castro <malves-b@c1r8s4.42porto.com>
2025-02-12 19:53:15 +0000

    checkpoint

---
+++ includes/cub.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malves-b <malves-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 13:14:39 by malves-b          #+#    #+#             */
+/*   Updated: 2025/02/12 19:33:02 by malves-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	CUB_H
+# define CUB_H
+
+# include <math.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/time.h>
+# include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "./minilibx-linux/mlx.h"
+
+typedef struct s_main
+{
+	char	**map; //-----//
+	void	*mlx;
+	void	*mlx_win;
+	void	*img;
+}t_main;
+
+
+#endif

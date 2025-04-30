c9e5da4497ca45a64f380f1e5d519e338899eebe
Jessica Silva De Oliveira Dallo <jesilva-@c2r1s5.42porto.com>
2025-04-30 15:38:54 +0100

    fix: correct invalid character handling

--- srcs/parse/validate_map.c
+++ srcs/parse/validate_map.c
@@ -31,12 +31,12 @@ bool	val_map(t_parse *parse)
 				printf("Error\nInvalid map player is duplicated!\n");
 				return false;
 			}
-			if(parse->map[x][y] != 'N' && parse->map[x][y] != 'S'
+			else if(parse->map[x][y] != 'N' && parse->map[x][y] != 'S'
 				&& parse->map[x][y] != 'E' && parse->map[x][y] != 'W'
-				&& parse->map[x][y] != '0' && parse->map[x][y] != '1')
+				&& parse->map[x][y] != '0' && parse->map[x][y] != '1' && parse->map[x][y] != ' ')
 			{
 				printf("Error\nInvalid map! The map contains invalid characters!\n");
-				printf("O CARACTERE É %c\n", parse->map[x][y]);
+				printf("O CARACTERE É %c -\n", parse->map[x][y]);
 				printf("posição x == %i y == %i\n", x, y);
 				return false;
 			}
